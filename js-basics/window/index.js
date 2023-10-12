6b07bbd3b238d39fb82f57c3399fa7b7db587f3a
Jenil Padshala <jenil_padshala@srmap.edu.in>
2023-10-12 21:34:49 +0530

    window object

---
+++ js-basics/window/index.js
@@ -0,0 +1,18 @@
+// console.dir(window);
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+
+// console.log(window.scrollX);
+// console.log(window.scrollY);
+
+// console.log(window.location.href);
+// // window.location.href = "https://google.com";
+// console.log(window.location.hostname);
+// console.log(window.location.pathname);
+
+// const myButton = document.querySelector("#myButton");
+
+// myButton.addEventListener("click", ()=> window.print());
+
+let p = window.confirm("Are you 18+ ?");
+console.log(p);
