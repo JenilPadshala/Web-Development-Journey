f82db08c3ce879c42818d92ba620aa8b3cb50b1d
Jenil Padshala <jenil_padshala@srmap.edu.in>
2023-10-12 21:34:33 +0530

    improvised the program

--- js-basics/move-the-box/index.js
+++ js-basics/move-the-box/index.js
@@ -1,25 +1,29 @@
 const myDiv = document.getElementById("myDiv");
 const resetButton = document.getElementById("resetButton");
-
+const playgroundDiv = document.getElementById("playgroundDiv");
 window.addEventListener("keydown", move);
 let x=0;
 let y=0;
 function move(event){
     switch(event.key){
         case "ArrowDown":
-            y+=5;
+            y+=10;
+            if (y>400) y = 400;
             myDiv.style.top = y + "px";
             break;
         case "ArrowUp":
-            y-=5;
+            y-=10;
+            if (y<0) y = 0;
             myDiv.style.top = y + "px";
             break;
         case "ArrowRight":
-            x+=5;
+            x+=10;
+            if(x> 400) x = 400;
             myDiv.style.left = x + "px";
             break;
         case "ArrowLeft":
-            x-=5;
+            x-=10;
+            if(x<0) x = 0;
             myDiv.style.left = x + "px";
     }
 }
